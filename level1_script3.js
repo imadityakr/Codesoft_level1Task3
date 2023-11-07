b0a17c8db3daebeb890e700bd169bf6da6d15938
Aditya Kumar <116816791+imadityakr@users.noreply.github.com>
2023-11-07 08:23:44 +0530

    final

---
+++ level1_script3.js
@@ -0,0 +1,50 @@
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    updateDisplay();
+}
+
+function appendOperator(op) {
+    if (operator === '') {
+        previousInput = currentInput;
+        currentInput = '';
+        operator = op;
+    }
+    updateDisplay();
+}
+
+function calculateResult() {
+    let result;
+    switch (operator) {
+        case '+':
+            result = parseFloat(previousInput) + parseFloat(currentInput);
+            break;
+        case '-':
+            result = parseFloat(previousInput) - parseFloat(currentInput);
+            break;
+        case '*':
+            result = parseFloat(previousInput) * parseFloat(currentInput);
+            break;
+        case '/':
+            result = parseFloat(previousInput) / parseFloat(currentInput);
+            break;
+    }
+    currentInput = result.toString();
+    previousInput = '';
+    operator = '';
+    updateDisplay();
+}
+
+function clearResult() {
+    currentInput = '';
+    operator = '';
+    previousInput = '';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    document.getElementById('result').value = currentInput;
+}
